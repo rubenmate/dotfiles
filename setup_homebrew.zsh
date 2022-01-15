a262c612edee3a38dfe01c490262da27997552bf
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-01-15 20:49:48 +0100

    Add mas-cli for MacAppStore Apps

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting Homebrew Setup >>>\n"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo "\n<<< Starting Homebrew Setup >>>\n"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle --verbose
 

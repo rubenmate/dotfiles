6684e185e671ec4d694698d5ccceda59843def91
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-01-15 13:39:14 +0100

    Add setup_homebrew.zsh and run with Dotbot

---
+++ setup_homebrew.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+brew install httpie
+# brew install htop
+# brew install ncdu
+brew install bat
+brew install fzf
+brew install neovim
+brew install ripgrep
+brew install tmux
+brew install tmuxinator
+brew install fd
+brew install rust
+brew install melonamin/formulae/trex
+brew install --cask kitty
+brew install --cask alfred
+brew install --cask alt-tab
+brew install --cask browserosaurus
+brew install --cask aldente
+brew install --cask hiddenbar
+brew install --cask firefox
+brew install --cask telegram
+brew install --cask enpass
+

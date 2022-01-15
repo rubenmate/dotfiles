dc493ee46098094c97455dc3400e1bf14d575207
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-01-15 14:21:56 +0100

    Switch to Brewfile

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -3,26 +3,5 @@
 echo "\n<<< Starting Homebrew Setup >>>\n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Packages to install:
-brew install httpie
-# brew install htop
-# brew install ncdu
-brew install bat
-brew install fzf
-brew install neovim
-brew install ripgrep
-brew install tmux
-brew install tmuxinator
-brew install fd
-brew install rust
-brew install --no-quarantine melonamin/formulae/trex
-brew install --no-quarantine --cask kitty
-brew install --no-quarantine --cask alfred
-brew install --no-quarantine --cask alt-tab
-brew install --no-quarantine --cask browserosaurus
-brew install --no-quarantine --cask aldente
-brew install --no-quarantine --cask hiddenbar
-brew install --no-quarantine --cask firefox
-brew install --no-quarantine --cask telegram
-brew install --no-quarantine --cask enpass
+brew bundle --verbose
 

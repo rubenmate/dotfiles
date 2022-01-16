66972f979b917780d1ff3d250340a43008a21f35
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-01-16 18:37:26 +0100

    Add brew bundle dump and alias and restructure aliases file

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -5,26 +5,26 @@ alias vim='nvim'
 # An alias to copy the curren working directory to clipboard
 alias cpwd='pwd | pbcopy'
 
-# brainstormr=~/Projects/development/planetargon/brainstormr
-# cd $brainstormr
-
 # Substitute ls with exa
-# general use
 alias ls='exa -laFh --git'                                                          # ls
 alias l='exa'                                                # list, size, type, git
 alias ll='exa -lbGF --git'                                             # long list
 alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
 alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
 alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-
-# specialty views
 alias lS='exa -1'                                                              # one column, just names
 alias lt='exa --tree --level=2'                                         # tree
 
+# Uses batman instead of man for manpages syntax highlighting
 alias man=batman
 
+# Alias for brew bundle dump
+alias bbd='brew bundle dump --force --describe'
+
+# brainstormr=~/Projects/development/planetargon/brainstormr
+# cd $brainstormr
+
 # Functions
-#
 # Create a new directory and enter it
 function mkcd() {
     mkdir -p "$@" && cd "$_"

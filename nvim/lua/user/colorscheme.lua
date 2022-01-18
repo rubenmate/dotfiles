7364b1e057c0ca76a685e069a76a401cb9f28301
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-01-19 00:00:57 +0100

    Update colorscheme check

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -15,13 +15,11 @@
 --
 
 -- Set your prefered colorscheme
-local colorscheme = "dracula"
-
--- If your prefered colorscheme is not found then it will continue
--- after notifying it
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
-if not status_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
-end
+vim.cmd [[
+try
+  colorscheme dracula
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]

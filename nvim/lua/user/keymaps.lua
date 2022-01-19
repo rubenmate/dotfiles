5cb5e08122ad7498381a76877cd0291c7eb54cad
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-01-19 02:12:42 +0100

    Update keybindings

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -34,8 +34,8 @@ keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize  -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize  +2<CR>", opts)
 
@@ -46,6 +46,10 @@ keymap("n", "<leader>=", ":wincmd =<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 
 -- BufferLine
 -- BufDel

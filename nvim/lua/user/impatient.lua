50663bbcb32ea04042fa87c41c8ead8be1c70912
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-02-03 14:45:41 +0100

    Add comments for impatient usage

--- nvim/lua/user/impatient.lua
+++ nvim/lua/user/impatient.lua
@@ -3,4 +3,10 @@ if not status_ok then
 	return
 end
 
+-- :LuaCacheClear:
+-- Remove the loaded cache and delete the cache file. A new cache file will be created the next time you load Neovim.
+-- :LuaCacheLog:
+-- View log of impatient.
+-- :LuaCacheProfile:
+-- View profiling data.
 impatient.enable_profile()

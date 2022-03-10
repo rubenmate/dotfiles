3d0a8f0d80052688e12f8bd891f90573a24a45e8
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-03-10 16:53:15 +0100

    Migrate Nvim-tree to setup function

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -71,7 +71,12 @@ lualine.setup {
 		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = {
+			"alpha",
+			"dashboard",
+			"NvimTree",
+			"Outline",
+		},
 		always_divide_middle = true,
 	},
 	sections = {
--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -1,3 +1,4 @@
+-- TODO: Migrate everything here to setup
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 vim.g.nvim_tree_icons = {
@@ -20,12 +21,10 @@ vim.g.nvim_tree_icons = {
 		symlink = "",
 	},
 }
-vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_group_empty = 1
-vim.g.lsp_diagnostics = 1
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
@@ -40,6 +39,7 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+	lsp_diagnostics = true,
 	disable_netrw = true,
 	hijack_netrw = true,
 	open_on_setup = false,
@@ -51,6 +51,11 @@ nvim_tree.setup {
 	update_to_buf_dir = {
 		enable = true,
 		auto_open = true,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = true,
+		},
 	},
 	diagnostics = {
 		enable = true,

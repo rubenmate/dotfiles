ad1d7cbdf9cea6976fc155edea235a117ff4c670
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-03-10 16:52:47 +0100

    Format changes

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -8,20 +8,25 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup({
+null_ls.setup {
 	debug = false,
 	-- TODO: Test if prefer local formats using the project settings and if not
 	-- found uses the default settings https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua.with({ extra_args = { "--config-path", vim.fn.expand('~/.config/stylua.toml') } }),
+		formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
+		formatting.black.with { extra_args = { "--fast" } },
+		formatting.stylua.with { extra_args = { "--config-path", vim.fn.expand "~/.config/stylua.toml" } },
 		diagnostics.flake8,
 	},
-  -- This is for formatting on save
+	-- This is for formatting on save
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function()
+					vim.lsp.buf.formatting_sync()
+				end,
+				buffer = 0,
+			})
 		end
 	end,
-})
+}

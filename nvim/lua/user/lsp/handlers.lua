3fe3b70b7ddf78ee405281cf5f9e5c6cc58494d2
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-03-16 11:24:47 +0100

    Add zeta_note and format fixes to LSP folder

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -62,7 +62,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
+	if client.name == "tsserver" or client.name == "jsonls" then
 		client.resolved_capabilities.document_formatting = false
 	end
 	lsp_keymaps(bufnr)
---
+++ nvim/lua/user/lsp/settings/zeta_note.lua
@@ -0,0 +1,5 @@
+-- Refer to:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#zeta_note
+return {
+	cmd = { "/Users/rubenmate/.local/bin/zeta-note" },
+}

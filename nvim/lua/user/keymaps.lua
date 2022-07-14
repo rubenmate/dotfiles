05d5ee0c4a019a5836cecf27e4fd2e29b4dac9b4
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-14 14:10:49 +0200

    Change nvim-cmp keymaps and remove unused ones

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -56,10 +56,6 @@ keymap("n", "<leader>=", ":wincmd =<CR>", opts)
 keymap("n", "<S-l>", "<cmd>bnext<CR>", opts)
 keymap("n", "<S-h>", "<cmd>bprevious<CR>", opts)
 
--- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
-
 -- BufferLine
 keymap("n", "<leader>db", "<cmd>Bdelete<CR>", opts)
 
@@ -100,16 +96,12 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation

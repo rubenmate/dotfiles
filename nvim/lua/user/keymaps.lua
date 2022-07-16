c273fdd58d2bab1e55735873bb43a7e5280a8d25
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-16 13:50:40 +0200

    adjust stuff

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -39,8 +39,8 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("i", "<A-a>", "<ESC>la", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize  -2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize  +2<CR>", opts)
 
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -133,13 +133,11 @@ return packer.startup(function(use)
     use "saadparwaiz1/cmp_luasnip" -- snippet completions
     use "hrsh7th/cmp-nvim-lsp"
     use "hrsh7th/cmp-nvim-lua" -- neovim Lua API
-    use "andersevenrud/cmp-tmux" -- tmux completion
     use "David-Kunz/cmp-npm" -- npm packages completion
     use { "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" }
 
     -- Snippets
     use "L3MON4D3/LuaSnip" --snippet engine
-    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
     -- LSP
     use "neovim/nvim-lspconfig" -- enable LSP

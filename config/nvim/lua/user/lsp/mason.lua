7683983a96742c999aba7adbc1a907b5e0111ed7
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-08-05 23:40:24 +0200

    feat: add rust-tools debugging and rename lsp-installer to mason

--- config/nvim/lua/user/lsp/mason.lua
+++ config/nvim/lua/user/lsp/mason.lua
@@ -34,11 +34,22 @@ mason_lspconfig.setup_handlers {
             capabilities = capabilities,
         }
     end,
+    -- TODO: Extract this options to a settings folder
     -- Next, you can provide targeted overrides for specific servers.
     ["rust_analyzer"] = function()
         -- I don't do anything here since the rust_analyzer LSP will be started by rust-tools which
         -- is required at init.lua
+        local extension_path = vim.env.HOME
+            .. "/.local/share/nvim/mason/packages/codelldb/extension/"
+        local codelldb_path = extension_path .. "adapter/codelldb"
+        local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
         require("rust-tools").setup {
+            dap = {
+                adapter = require("rust-tools.dap").get_codelldb_adapter(
+                    codelldb_path,
+                    liblldb_path
+                ),
+            },
             server = {
                 on_attach = on_attach,
                 capabilities = capabilities,
@@ -59,6 +70,30 @@ mason_lspconfig.setup_handlers {
                             [vim.fn.expand "$VIMRUNTIME/lua"] = true,
                             [vim.fn.stdpath "config" .. "/lua"] = true,
                         },
+                    },
+                },
+            },
+        }
+    end,
+    ["emmet_ls"] = function()
+        local emmet_capabilities = capabilities
+        emmet_capabilities.textDocument.completion.completionItem.snippetSupport = true
+        require("lspconfig")["emmet_ls"].setup {
+            capabilities = emmet_capabilities,
+            filetypes = {
+                "html",
+                "typescriptreact",
+                "javascriptreact",
+                "css",
+                "sass",
+                "scss",
+                "less",
+            },
+            init_options = {
+                html = {
+                    options = {
+                        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                        ["bem.enabled"] = true,
                     },
                 },
             },

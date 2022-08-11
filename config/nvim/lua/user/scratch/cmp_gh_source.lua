c34ae0359ec54e462055323c0fb9d17aa8b123d0
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-08-11 18:11:54 +0200

    fix: error on gh_source when git repo doesn't have remote

--- config/nvim/lua/user/scratch/cmp_gh_source.lua
+++ config/nvim/lua/user/scratch/cmp_gh_source.lua
@@ -32,6 +32,7 @@ source.complete = function(self, _, callback)
                     local ok, parsed = pcall(vim.json.decode, table.concat(result, ""))
                     if not ok then
                         vim.notify "Failed to parse gh result"
+                        -- vim.notify "Failed to parse gh result"
                         return
                     end
 

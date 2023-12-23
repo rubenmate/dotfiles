e2adddf731903404124d9e851561bc57910f39d5
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2023-12-23 18:06:00 +0100

    incline.nvim config change

--- roles/neovim/files/nvim/lua/plugins/ui.lua
+++ roles/neovim/files/nvim/lua/plugins/ui.lua
@@ -1,6 +1,12 @@
 return {
     {
         "b0o/incline.nvim", -- Floating statuslines (filename)
-        opts = {},
+        opts = {
+            hide = {
+                -- If `"focused_win"`, the Incline statusline for the focused window 
+                -- will be hidden if the cursor is on the same line.
+                cursorline = "focused_win"
+            },
+        },
     }
 }

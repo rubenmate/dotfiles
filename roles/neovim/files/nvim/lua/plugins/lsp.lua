28035a34004453296097f0351697de7eebfa82d4
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2023-12-26 00:12:07 +0100

    add extra lsp binds

--- roles/neovim/files/nvim/lua/plugins/lsp.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "gopls"  }
+                ensure_installed = { "lua_ls", "tsserver", "gopls" }
             })
         end
     },
@@ -22,12 +22,17 @@ return {
             lspconfig.tsserver.setup({})
             lspconfig.lua_ls.setup({})
             lspconfig.gopls.setup({})
+            -- Add more LSPs here
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
             vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
         end
     }
 }

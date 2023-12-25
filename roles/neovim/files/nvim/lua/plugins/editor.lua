7878727edfacf4c54c274eaf8e4c99eabdd5ace2
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2023-12-25 21:53:57 +0100

    add gitsigns plugin

--- roles/neovim/files/nvim/lua/plugins/editor.lua
+++ roles/neovim/files/nvim/lua/plugins/editor.lua
@@ -16,4 +16,9 @@ return {
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
+    { "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup()
+        end,
+    }
 }

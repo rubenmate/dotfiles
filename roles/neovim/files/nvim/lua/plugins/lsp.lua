51bc25808324e3052001e3bb6ec869acdf878aa4
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2023-12-28 07:16:32 +0100

    add none-ls formatter

--- roles/neovim/files/nvim/lua/plugins/lsp.lua
+++ roles/neovim/files/nvim/lua/plugins/lsp.lua
@@ -35,4 +35,19 @@ return {
             vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
         end
     }
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.prettierd,
+                    null_ls.builtins.diagnostics.eslint_d,
+                },
+            })
+
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
+    },
 }

74afdcee81734b033848f310e8931db7e4e283d6
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-03-06 15:39:41 +0100

    feat(neovim): add formatting plugins

---
+++ roles/neovim/files/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,37 @@
+return {
+	-- NOTE: Currently using conform.nvim, this could be achieved also using none-ls/null-ls
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- Use a sub-list to run only the first available formatter
+			javascript = { { "biome", "prettierd", "prettier" } },
+			typescript = { { "biome", "prettierd", "prettier" } },
+			java = { { "google-java-format" } },
+		},
+		format_on_save = {
+			-- timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		-- This code above is a shortcut for:
+		-- vim.api.nvim_create_autocmd("BufWritePre", {
+		-- pattern = "*",
+		-- callback = function(args)
+		-- require("conform").format({ bufnr = args.buf })
+		-- end,
+		-- })
+	},
+}

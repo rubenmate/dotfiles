87e756043a1a67cf4102039300c233f18eae4c70
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-03-21 17:12:41 +0100

    feat(neovim): configure biome por jsx and tsx files

--- roles/neovim/files/nvim/lua/plugins/formatting.lua
+++ roles/neovim/files/nvim/lua/plugins/formatting.lua
@@ -20,6 +20,8 @@ return {
 			-- Use a sub-list to run only the first available formatter
 			javascript = { { "biome", "prettierd", "prettier" } },
 			typescript = { { "biome", "prettierd", "prettier" } },
+			javascriptreact = { { "biome", "prettierd", "prettier" } },
+			typescriptreact = { { "biome", "prettierd", "prettier" } },
 			java = { { "google-java-format" } },
 		},
 		format_on_save = {

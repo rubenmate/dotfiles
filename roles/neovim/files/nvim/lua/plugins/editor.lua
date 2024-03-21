acc371e9c5c03a55528c0f4dc82af1dcfb9c25c3
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-03-21 17:12:10 +0100

    feat(neovim): add trouble and todo comments

--- roles/neovim/files/nvim/lua/plugins/editor.lua
+++ roles/neovim/files/nvim/lua/plugins/editor.lua
@@ -58,4 +58,14 @@ return {
 			},
 		},
 	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
 }

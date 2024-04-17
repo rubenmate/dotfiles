5a177f0549909599c00a6129f0f8311a321b416f
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-04-17 12:12:04 +0200

    fix(neovim): hurl config incorrect

--- roles/neovim/files/nvim/lua/plugins/plugins.lua
+++ roles/neovim/files/nvim/lua/plugins/plugins.lua
@@ -1,41 +1,38 @@
 return {
-	"jellydn/hurl.nvim",
-	dependencies = { "MunifTanjim/nui.nvim" },
-	ft = "hurl",
-	opts = {
-		-- Show debugging info
-		debug = false,
-		-- Show notification on run
-		show_notification = false,
-		-- Show response in popup or split
-		mode = "split",
-		-- Default formatter
-		formatters = {
-			json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
-			html = {
-				"prettier", -- Make sure you have install prettier in your system, e.g: npm install -g prettier
-				"--parser",
-				"html",
+	{
+		"jellydn/hurl.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			-- Show debugging info
+			debug = false,
+			-- Show notification on run
+			show_notification = false,
+			-- Show response in popup or split
+			mode = "split",
+			-- Default formatter
+			formatters = {
+				json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
+				html = {
+					"prettier", -- Make sure you have install prettier in your system, e.g: npm install -g prettier
+					"--parser",
+					"html",
+				},
 			},
 		},
-		env_file = {
-			"hurl.env",
+		keys = {
+			-- Run API request
+			{ "<leader>A", "<cmd>HurlRunner<CR>", desc = "Run All requests" },
+			{ "<leader>a", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
+			{ "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
+			{ "<leader>tm", "<cmd>HurlToggleMode<CR>", desc = "Hurl Toggle Mode" },
+			{ "<leader>tv", "<cmd>HurlVerbose<CR>", desc = "Run Api in verbose mode" },
+			-- Run Hurl request in visual mode
+			{ "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
 		},
-	},
-	keys = {
-		-- Run API request
-		{ "<leader>A", "<cmd>HurlRunner<CR>", desc = "Run All requests" },
-		{ "<leader>a", "<cmd>HurlRunnerAt<CR>", desc = "Run Api request" },
-		{ "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
-		{ "<leader>tm", "<cmd>HurlToggleMode<CR>", desc = "Hurl Toggle Mode" },
-		{ "<leader>tv", "<cmd>HurlVerbose<CR>", desc = "Run Api in verbose mode" },
-		-- Run Hurl request in visual mode
-		{ "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v" },
-	},
-	{
-		"NStefan002/2048.nvim",
-		cmd = "Play2048",
-		config = true,
 	},
 	{
 		"NeogitOrg/neogit",
--- roles/neovim/files/nvim/lua/plugins/treesitter.lua
+++ roles/neovim/files/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "go", "lua", "javascript" },
+			ensure_installed = { "go", "lua", "javascript", "hurl" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})

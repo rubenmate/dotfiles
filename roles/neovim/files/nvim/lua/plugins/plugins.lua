b8447d2616031f299d2f890ddac73875610431a2
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-06-24 15:31:05 +0200

    feat(neovim): add refactoring plugin

--- roles/neovim/files/nvim/lua/plugins/plugins.lua
+++ roles/neovim/files/nvim/lua/plugins/plugins.lua
@@ -87,4 +87,20 @@ return {
 		},
 		cmd = "UndotreeToggle",
 	},
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("refactoring").setup()
+			-- load refactoring Telescope extension
+			require("telescope").load_extension("refactoring")
+
+			vim.keymap.set({ "n", "x" }, "<leader>rr", function()
+				require("telescope").extensions.refactoring.refactors()
+			end)
+		end,
+	},
 }
